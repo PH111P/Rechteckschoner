f2bdcb5d0ccb0be7e34d3726df3747ae4d493596
PH111P@Arch <philipwellnitz@gmx.de>
2014-08-02 16:55:43 +0000

    added bitmap.h

---
+++ bitmap.h
@@ -0,0 +1,26 @@
+#pragma once
+
+typedef unsigned char u8;
+struct pixel{
+public:
+  u8 red;
+  u8 green;
+  u8 blue;
+};
+
+#define SCALE(x, mx) (((x) < 0) ? 0 : ((int)(256.0*((double)(x)/(double)(mx)))))
+
+struct bitmap{
+private:
+  pixel** pixels;
+  size_t width;
+  size_t height;
+public:
+  bitmap(size_t width, size_t height);
+  
+  pixel& operator()(int x, int y);
+  pixel operator()(int x, int y) const;
+  
+  int writeToFile(const char* path) const;
+};
+
--- rectangle.h
+++ rectangle.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <vector>
 
+struct bitmap;
+
 class rectangle {
 private:
   int posx,		//x position of the top-left corner
@@ -8,18 +10,20 @@ private:
       width,		//rectangle's width
       height;		//rectangle's height
   
+  int numChildren;	//number of max enclosed rectangles
+  
   std::vector<rectangle*> children;
 public:
   static int DistanceBetweenRectangles = 10;
   
-  int getX ();
-  int getY ();
-  int getWidth ();
-  int getHeight ();
+  int getX () const;
+  int getY () const;
+  int getWidth () const;
+  int getHeight () const;
   
-  rectangle(int posX, int posY, int rectWidth, int rectHeight)
-    : posx(posX), posy(posY), width(rectWidth), height(rectHeight) { }
+  rectangle(int posX, int posY, int rectWidth, int rectHeight, int maxChildren)
+    : posx(posX), posy(posY), width(rectWidth), height(rectHeight), numChildren(maxChildren) { }
   
   void construct();
-  void draw( ... );
+  void draw( bitmap* res ) const;
 };
