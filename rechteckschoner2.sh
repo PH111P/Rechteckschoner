eac67159520a467f4547695d8bb97dac7c760d46
PH111P@Arch <philipwellnitz@gmx.de>
2014-08-07 16:02:22 +0000

    added cron script

---
+++ rechteckschoner2.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# This is a version for cron
+
+./rechteckschoner -w 1366 -h 768 -d 10 -b 8 -c 3 -t .tmp -o ./ 	#use generated tmp and change only parts
+feh  --bg-scale './rechteckschoner.png' 
