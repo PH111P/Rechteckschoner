858c739154fb31bfdf5ecb11dd803e7875d246b3
PH111P@Arch <philipwellnitz@gmx.de>
2014-08-07 18:41:01 +0000

    some bfs

--- rechteckschoner.sh
+++ rechteckschoner.sh
@@ -2,9 +2,15 @@
 
 # Use the script in this file to bring life to your desktop!
 
-./rechteckschoner -w 1366 -h 768 -d 10 -b 8 -c 3 -o ./ 			#init call, generate .tmp
+/home/philip/Projects/Rechteckschoner/rechteckschoner -w 1366 -h 768 -d 12 -b 3 -c 3 -o /home/philip/Projects/Rechteckschoner/ -i			#init call, generate .tmp
+feh  --bg-scale '/home/philip/Projects/Rechteckschoner/rechteckschoner.png'
+
 while true; do
-	sleep 5s
-	feh  --bg-scale './rechteckschoner.png'
-	./rechteckschoner -w 1366 -h 768 -d 10 -b 8 -c 3 -t .tmp -o ./ 	#use generated tmp and change only parts
+	sleep 2s
+	/home/philip/Projects/Rechteckschoner/rechteckschoner -w 1366 -h 768 -d 12 -b 3 -c 3 -r -t /home/philip/Projects/Rechteckschoner/.tmp -o  /home/philip/Projects/Rechteckschoner/	#use generated tmp and change only parts
+	feh  --bg-scale '/home/philip/Projects/Rechteckschoner/rechteckschoner.png'
+	
+	sleep 30s
+	/home/philip/Projects/Rechteckschoner/rechteckschoner -w 1366 -h 768 -d 12 -b 3 -c 3 -i -t /home/philip/Projects/Rechteckschoner/.tmp -o /home/philip/Projects/Rechteckschoner/	
+	feh  --bg-scale '/home/philip/Projects/Rechteckschoner/rechteckschoner.png'
 done
--- rechteckschoner2.sh
+++ rechteckschoner2.sh
@@ -2,5 +2,5 @@
 
 # This is a version for cron
 
-./rechteckschoner -w 1366 -h 768 -d 10 -b 8 -c 3 -t .tmp -o ./ 	#use generated tmp and change only parts
+./rechteckschoner -w 1366 -h 768 -d 12 -b 3 -c 3 -t .tmp -o ./ 	#use generated tmp and change only parts
 feh  --bg-scale './rechteckschoner.png' 
