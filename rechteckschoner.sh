4772c7f5423ccc1ed531d5a9a3bc3a122de5b3da
Philip Wellnitz <philipwellnitz@gmx.de>
2014-08-12 18:43:08 +0000

    Update rechteckschoner.sh

--- rechteckschoner.sh
+++ rechteckschoner.sh
@@ -2,15 +2,15 @@
 
 # Use the script in this file to bring life to your desktop!
 
-/home/philip/Projects/Rechteckschoner/rechteckschoner -w 1366 -h 768 -d 12 -b 3 -c 3 -o /home/philip/Projects/Rechteckschoner/ -i			#init call, generate .tmp
-feh  --bg-scale '/home/philip/Projects/Rechteckschoner/rechteckschoner.png'
+~/Projects/Rechteckschoner/rechteckschoner -w 1366 -h 768 -d 12 -b 3 -c 3 -o ~/Projects/Rechteckschoner/ -i			#init call, generate .tmp
+feh  --bg-scale '~/Projects/Rechteckschoner/rechteckschoner.png'
 
 while true; do
 	sleep 2s
-	/home/philip/Projects/Rechteckschoner/rechteckschoner -w 1366 -h 768 -d 12 -b 3 -c 3 -r -t /home/philip/Projects/Rechteckschoner/.tmp -o  /home/philip/Projects/Rechteckschoner/	#use generated tmp and change only parts
-	feh  --bg-scale '/home/philip/Projects/Rechteckschoner/rechteckschoner.png'
+	~/Projects/Rechteckschoner/rechteckschoner -w 1366 -h 768 -d 12 -b 3 -c 3 -r -t ~/Projects/Rechteckschoner/.tmp -o  /home/philip/Projects/Rechteckschoner/	#use generated tmp and change only parts
+	feh  --bg-scale '~/Projects/Rechteckschoner/rechteckschoner.png'
 	
 	sleep 30s
-	/home/philip/Projects/Rechteckschoner/rechteckschoner -w 1366 -h 768 -d 12 -b 3 -c 3 -i -t /home/philip/Projects/Rechteckschoner/.tmp -o /home/philip/Projects/Rechteckschoner/	
-	feh  --bg-scale '/home/philip/Projects/Rechteckschoner/rechteckschoner.png'
+	~/Projects/Rechteckschoner/rechteckschoner -w 1366 -h 768 -d 12 -b 3 -c 3 -i -t ~/Projects/Rechteckschoner/.tmp -o /home/philip/Projects/Rechteckschoner/	
+	feh  --bg-scale '~/Projects/Rechteckschoner/rechteckschoner.png'
 done
